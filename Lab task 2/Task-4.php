18de742d1b3a022067eb9cf7052f545520d6a7a0
helenchora <helenchora43@gmail.com>
2023-02-18 00:17:49 +0600

    f

---
+++ Lab task 2/Task-4.php
@@ -0,0 +1,14 @@
+<html>
+<title>Lab -2</title>
+</head>  
+<body>  
+
+<?php
+for($i=1; $i<=100; $i++){
+    if($i%2 == 0)
+    {  
+        echo "$i is an Even number.<br />";  
+    }else{  
+        echo "$i is Odd number.<br />";  
+    }  
+}  
---
+++ Lab task 2/Task-5.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Lab-2</title>
+        <body>
+            <?php
+
+            $students = [
+                            's1'=>['id'=>10, 'name'=>'Chora', 'cgpa'=>3.8],
+                            's2'=>['id'=>20, 'name'=>'Helen', 'cgpa'=>3.9],
+                            's3'=>['id'=>30, 'name'=>'Tink', 'cgpa'=>2.5]
+                        ];
+           echo $students['s2']['name'];
+
+
+            ?>
+            </body>
+            </html>
